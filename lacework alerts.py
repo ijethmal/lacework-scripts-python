fc71fe08dad3d97863831dcba0e78832e54d11c6
Isha <s10205038@connect.np.edu.sg>
2023-03-20 18:51:50 +0800

    first

---
+++ lacework alerts.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import laceworktoken
+
+account = "secureauth.lacework.net"
+
+token_body = {
+  "paging": {
+    "rows": 1000,
+    "totalRows": 3120,
+    "urls": {
+      "nextPage": "https://YourLacework.lacework.net/api/v2/Alerts/AbcdEfgh123..."
+    }
+  },
+  "data": [
+    {
+      "startTime": "2023-03-18",
+      "severity": "High",
+      "endTime": "2023-03-20"
+    }
+  ]
+}
+
+jsontoken_body = json.dumps(token_body)
+print(jsontoken_body)
+
+r = requests.get("https://secureauth.lacework.net/api/v2/Alerts", headers={"Authorization": "Bearer {}".format(laceworktoken.token), "Content-Type": "application/json"}, json=jsontoken_body)
+
+print(r)
+
+response = r.json()['data'][0]
+
+print(response)
