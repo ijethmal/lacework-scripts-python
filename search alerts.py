20d164e2fa97f5c53786ae1be4c19d61f98be6ba
Isha <s10205038@connect.np.edu.sg>
2023-03-27 23:19:09 +0800

    new script

--- search alerts.py
+++ search alerts.py
@@ -6,8 +6,8 @@ import laceworktoken
 account = "secureauth.lacework.net"
 token_body = {
   "timeFilter": {
-    "startTime": "2023-03-13T08:00:00.000Z",
-    "endTime": "2023-03-20T08:00:00.000Z"
+    "startTime": "2023-03-25T08:00:00.000Z",
+    "endTime": "2023-03-26T08:00:00.000Z"
   },
   "filters": [
     {
